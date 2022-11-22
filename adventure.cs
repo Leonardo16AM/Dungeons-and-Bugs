99a6978ba20bfc98a17af21ddb044dc86279f92c
Leonardo16AM <leo16am@gmail.com>
2022-11-22 00:57:35 -0500

    Deleted inutile things

--- adventure.cs
+++ adventure.cs
@@ -1,11 +1,5 @@
 using Newtonsoft.Json;
 
-class hero{
-    public string name,real_name,historia,picture;
-    public int life,strength, agility,mana;
-}
-
-
 
 class adventure{
     public string adv;
@@ -16,6 +10,5 @@ class adventure{
         adv=name;
         json = File.ReadAllText($"adventures/{name}/berserk.json");
         file = JsonConvert.DeserializeObject(json);
-        Console.WriteLine(file.heroes[0][1]);
     }
 }
